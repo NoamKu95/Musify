d3d89defbe7d7c83f57a0b7f202569720e82c05c
noamkuMoDev <noamk@moveo.co.il>
2022-06-25 15:00:08 +0300

    Added API calls to fetch data - New Releases, Featured Playlists, Recommended Genres, Recommendations,

--- Musify/Musify/Controllers/Main Screens/HomeViewController.swift
+++ Musify/Musify/Controllers/Main Screens/HomeViewController.swift
@@ -12,6 +12,60 @@ class HomeViewController : UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        getData()
+    }
+    
+    func getData() {
+        ApiCaller.shared.getNewReleases { result in
+            switch result {
+            case .success(let model):
+                //print(model)
+                break
+            case .failure(let error):
+                print(error.localizedDescription)
+                break
+            }
+        }
+        
+        ApiCaller.shared.getFeaturedPlaylists { result in
+            switch result {
+            case .success(let model):
+                //print(model)
+                break
+            case .failure(let error):
+                print(error.localizedDescription)
+                break
+            }
+        }
+        
+        ApiCaller.shared.getRecommendedGenres { result in
+            switch result {
+            case .success(let model):
+                let genres = model.genres
+                var seeds = Set<String>()
+                while seeds.count < 5 {
+                    if let random = genres.randomElement() {
+                        seeds.insert(random)
+                    }
+                }
+                
+                ApiCaller.shared.getRecommendations(genres: seeds) { result in
+                    switch result {
+                    case .success(let model):
+                        //print(model)
+                        break
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                        break
+                    }
+                }
+                
+                break
+            case .failure(let error):
+                print(error.localizedDescription)
+                break
+            }
+        }
     }
     
     @IBAction func profileTapped(_ sender: UIButton) {
--- Musify/Musify/Managers/ApiCaller.swift
+++ Musify/Musify/Managers/ApiCaller.swift
@@ -23,7 +23,7 @@ class ApiCaller {
     private init() {}
     
     public func getCurrentUserProfile(completionHandler: @escaping (Result<UserProfile, Error>) -> ()) {
-        createRequest(with: URL(string: Constants.API.PROFILE_BASE_API_URL + "/me"), ofType: .GET) { baseRequest in
+        createRequest(with: URL(string: Constants.API.REQUESTS_BASE_API_URL + "/me"), ofType: .GET) { baseRequest in
             let task = URLSession.shared.dataTask(with: baseRequest) { data, _, error in
                 guard let data = data, error == nil else {
                     completionHandler(.failure(APIError.failedToGetData))
@@ -40,6 +40,77 @@ class ApiCaller {
             task.resume()
         }
     }
+    
+    public func getNewReleases(completionHandler: @escaping (Result<NewReleasesResponse,Error>) -> ()) {
+        createRequest(with: URL(string: Constants.API.REQUESTS_BASE_API_URL + "/browse/new-releases?limit=50"), ofType: .GET) { request in
+            let task = URLSession.shared.dataTask(with: request) { data, _, error in
+                guard let data = data, error == nil else {
+                    return
+                }
+                do {
+                    let result = try JSONDecoder().decode(NewReleasesResponse.self, from: data)
+                    completionHandler(.success(result))
+                } catch {
+                    completionHandler(.failure(error))
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    public func getFeaturedPlaylists(completionHandler: @escaping (Result<FeaturedPlaylistResponse,Error>) -> ()) {
+        createRequest(with: URL(string: Constants.API.REQUESTS_BASE_API_URL + "/browse/featured-playlists?limit=50"), ofType: .GET) { request in
+            let task = URLSession.shared.dataTask(with: request) { data, _, error in
+                guard let data = data, error == nil else {
+                    return
+                }
+                do {
+                    let result = try JSONDecoder().decode(FeaturedPlaylistResponse.self, from: data)
+                    completionHandler(.success(result))
+                } catch {
+                    completionHandler(.failure(error))
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    public func getRecommendedGenres(completionHandler: @escaping (Result<RecommendedGenresResponse,Error>) -> ()) {
+        createRequest(with: URL(string: Constants.API.REQUESTS_BASE_API_URL + "/recommendations/available-genre-seeds"), ofType: .GET) { request in
+            let task = URLSession.shared.dataTask(with: request) { data, _, error in
+                guard let data = data, error == nil else {
+                    return
+                }
+                do {
+                    let result = try JSONDecoder().decode(RecommendedGenresResponse.self, from: data)
+                    completionHandler(.success(result))
+                } catch {
+                    completionHandler(.failure(error))
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    public func getRecommendations(genres: Set<String>, completionHandler: @escaping (Result<RecommendationsResponse,Error>) -> ()) {
+        
+        let seeds = genres.joined(separator: ",")
+        createRequest(with: URL(string: Constants.API.REQUESTS_BASE_API_URL + "/recommendations?seed_genres=\(seeds)"), ofType: .GET) { request in
+            let task = URLSession.shared.dataTask(with: request) { data, _, error in
+                guard let data = data, error == nil else {
+                    return
+                }
+                do {
+                    let result = try JSONDecoder().decode(RecommendationsResponse.self, from: data)
+                    completionHandler(.success(result))
+                } catch {
+                    completionHandler(.failure(error))
+                }
+            }
+            task.resume()
+        }
+    }
+    
     
     private func createRequest(with url: URL?, ofType type: HTTPMethod, completiongHandler: @escaping (URLRequest) -> ()) {
         guard let apiURL = url else {
