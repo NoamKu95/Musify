66a3d88e3b43e55d2c0d0cd2e21850945490f83c
noamkuMoDev <noamk@moveo.co.il>
2022-07-02 14:42:17 +0300

    Created 3 cells to display data on homepage

---
+++ Musify/Musify/Views/HomepageCells/FeaturedPlaylistCollectionViewCell.swift
@@ -0,0 +1,83 @@
+//
+//  FeaturedPlaylistCollectionViewCell.swift
+//  Musify
+//
+//  Created by Noam Kurtzer on 30/06/2022.
+//
+
+import UIKit
+
+class FeaturedPlaylistCollectionViewCell: UICollectionViewCell {
+    static let identifier = Constants.Cells.FEATURED_PLAYLIST
+    
+    private var playlistCoverImageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "photo")
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = 4
+        imageView.contentMode = .scaleToFill
+        return imageView
+    }()
+    
+    private let playlistNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let creatorNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 15, weight: .thin)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.backgroundColor = .secondarySystemBackground
+        contentView.addSubview(playlistCoverImageView)
+        contentView.addSubview(playlistNameLabel)
+        contentView.addSubview(creatorNameLabel)
+        contentView.clipsToBounds = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let imageSize = contentView.height - 70
+        playlistCoverImageView.frame = CGRect(x: (contentView.width - imageSize)/2,
+                                              y: 7,
+                                              width: imageSize,
+                                              height: imageSize)
+        
+        playlistNameLabel.frame = CGRect(x: 7,
+                                         y: contentView.height-60,
+                                         width: contentView.width-12,
+                                         height: 30)
+        
+        creatorNameLabel.frame = CGRect(x: 7,
+                                        y: contentView.height-35,
+                                        width: contentView.width-12,
+                                        height: 30)
+    }
+    
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        creatorNameLabel.text = nil
+        playlistNameLabel.text = nil
+        playlistCoverImageView.image = nil
+    }
+    
+    func configure(with viewModel: FeaturedPlaylistCellViewModel) {
+        playlistNameLabel.text = viewModel.name
+        creatorNameLabel.text = viewModel.creatorName
+        playlistCoverImageView.sd_setImage(with: viewModel.artworkURL, completed: nil)
+    }
+}
---
+++ Musify/Musify/Views/HomepageCells/NewReleaseCollectionViewCell.swift
@@ -0,0 +1,106 @@
+//
+//  NewReleaseCollectionViewCell.swift
+//  Musify
+//
+//  Created by Noam Kurtzer on 30/06/2022.
+//
+
+import UIKit
+import SDWebImage
+
+class NewReleaseCollectionViewCell: UICollectionViewCell {
+    static let identifier = Constants.Cells.NEW_RELEASES
+    
+    private var albumCoverImageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "photo")
+        imageView.contentMode = .scaleToFill
+        return imageView
+    }()
+    
+    private let albumNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let tracksNumberLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 18, weight: .thin)
+        //label.numberOfLines = 0
+        return label
+    }()
+    
+    private let artistNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 18, weight: .light)
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.backgroundColor = .secondarySystemBackground
+        contentView.addSubview(albumCoverImageView)
+        contentView.addSubview(artistNameLabel)
+        contentView.addSubview(tracksNumberLabel)
+        contentView.addSubview(albumNameLabel)
+        contentView.clipsToBounds = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        artistNameLabel.sizeToFit()
+        albumNameLabel.sizeToFit()
+        tracksNumberLabel.sizeToFit()
+        
+        // Album Image
+        let imageSize: CGFloat = contentView.frame.height - 10
+        albumCoverImageView.frame = CGRect(x: 5, y: 5, width: imageSize, height: imageSize)
+        
+        
+        // Album Name Label
+        let albumLabelSize = albumNameLabel.sizeThatFits(CGSize(width: contentView.width-imageSize-10,
+                                                                height: contentView.height-10))
+        albumNameLabel.frame = CGRect(x: albumCoverImageView.right + 10,
+                                      y: 5,
+                                      width: albumLabelSize.width,
+                                      height: min(60,albumLabelSize.height))
+        
+        
+        
+        // Artist Name Label
+        artistNameLabel.frame = CGRect(x: albumCoverImageView.right + 10,
+                                       y: albumNameLabel.bottom,
+                                       width: contentView.width - albumCoverImageView.right - 10,
+                                      height: 30)
+        
+        // Number of Tracks Label
+        tracksNumberLabel.frame = CGRect(x: albumCoverImageView.right + 10,
+                                         y: contentView.bottom - 44,
+                                         width: tracksNumberLabel.width,
+                                         height: 44)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        albumNameLabel.text = nil
+        artistNameLabel.text = nil
+        tracksNumberLabel.text = nil
+        albumCoverImageView.image = nil
+    }
+    
+    func configure(with viewModel: NewReleasesCellViewModel) {
+        albumNameLabel.text = viewModel.name
+        artistNameLabel.text = viewModel.artistName
+        tracksNumberLabel.text = "\(viewModel.numberOfTracks) track\(viewModel.numberOfTracks > 1 ? "s" : "")"
+        albumCoverImageView.sd_setImage(with: viewModel.artWorkURL, completed: nil)
+    }
+}
