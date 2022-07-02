50ada34728a79c46067826398e19c68a76bb86a5
noamkuMoDev <noamk@moveo.co.il>
2022-07-02 14:40:53 +0300

    Making 4 api calls and getting data on the homepage

--- Musify/Musify/Controllers/Main Screens/HomeViewController.swift
+++ Musify/Musify/Controllers/Main Screens/HomeViewController.swift
@@ -8,38 +8,104 @@
 import Foundation
 import UIKit
 
+enum BrowseSectionType {
+    case newReleases(viewModels: [NewReleasesCellViewModel])
+    case featuredPlaylists(viewModels: [FeaturedPlaylistCellViewModel])
+    case recommendedTracks(viewModels: [RecommendedTrackCellViewModel])
+}
+
 class HomeViewController : UIViewController {
+    
+    private var spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView()
+        spinner.tintColor = .label
+        spinner.hidesWhenStopped = true
+        return spinner
+    }()
+    
+    private var collectionView: UICollectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: UICollectionViewCompositionalLayout { sectionIndex, _ -> NSCollectionLayoutSection? in
+            return HomeViewController.createSectionLayout(section: sectionIndex)
+        }
+    )
+    
+    private var sections = [BrowseSectionType]()
+    private var newAlbums = [Album]()
+    private var playlists = [Playlist]()
+    private var tracks = [AudioTrack]()
+    private var clickedAlbum: Album?
+    private var clickedTrack: AudioTrack?
+    private var clickedPlaylist: Playlist?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        getData()
+        initialteUIElements()
+        fetchData()
     }
     
-    func getData() {
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
+    }
+    
+    
+    func initialteUIElements() {
+        configureCollectionView()
+        view.addSubview(spinner)
+    }
+    
+    func configureCollectionView() {
+        view.addSubview(collectionView)
+        collectionView.register(NewReleaseCollectionViewCell.self, forCellWithReuseIdentifier: NewReleaseCollectionViewCell.identifier)
+        collectionView.register(RecommendedTrackCollectionViewCell.self, forCellWithReuseIdentifier: RecommendedTrackCollectionViewCell.identifier)
+        collectionView.register(FeaturedPlaylistCollectionViewCell.self, forCellWithReuseIdentifier: FeaturedPlaylistCollectionViewCell.identifier)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.backgroundColor = .lightGray
+    }
+    
+    
+    func fetchData() {
+        let group = DispatchGroup()
+        group.enter()
+        group.enter()
+        group.enter()
+        
+        var newReleases: NewReleasesResponse?
+        var featuredPlaylist: FeaturedPlaylistResponse?
+        var recommendations: RecommendationsResponse?
+        
         ApiCaller.shared.getNewReleases { result in
+            defer {
+                group.leave()
+            }
             switch result {
             case .success(let model):
-                //print(model)
+                newReleases = model
                 break
             case .failure(let error):
                 print(error.localizedDescription)
                 break
             }
         }
-        
         ApiCaller.shared.getFeaturedPlaylists { result in
+            defer {
+                group.leave()
+            }
             switch result {
             case .success(let model):
-                //print(model)
+                featuredPlaylist = model
                 break
             case .failure(let error):
                 print(error.localizedDescription)
                 break
             }
         }
-        
         ApiCaller.shared.getRecommendedGenres { result in
             switch result {
+                
             case .success(let model):
                 let genres = model.genres
                 var seeds = Set<String>()
@@ -48,27 +114,216 @@ class HomeViewController : UIViewController {
                         seeds.insert(random)
                     }
                 }
-                
-                ApiCaller.shared.getRecommendations(genres: seeds) { result in
-                    switch result {
+                ApiCaller.shared.getRecommendations(genres: seeds) { recommenderdResult in
+                    defer {
+                        group.leave()
+                    }
+                    switch recommenderdResult {
                     case .success(let model):
-                        //print(model)
+                        recommendations = model
                         break
                     case .failure(let error):
                         print(error.localizedDescription)
                         break
                     }
                 }
-                
                 break
+                
             case .failure(let error):
                 print(error.localizedDescription)
                 break
             }
         }
+        
+        group.notify(queue: .main) {
+            guard let newAlbums = newReleases?.albums.items,
+                  let playlists = featuredPlaylist?.playlists.items,
+                  let tracks = recommendations?.tracks else {
+                return
+            }
+            self.configureModels(newAlbums: newAlbums, playlists: playlists, tracks: tracks)
+        }
+        
+    }
+    
+    private func configureModels(newAlbums: [Album], playlists: [Playlist], tracks: [AudioTrack]) {
+        
+        self.playlists = playlists
+        self.newAlbums = newAlbums
+        self.tracks = tracks
+        
+        sections.append(.newReleases(viewModels: newAlbums.compactMap({
+            return NewReleasesCellViewModel(name: $0.name,
+                                            artWorkURL: URL(string: $0.images.first?.url ?? ""),
+                                            numberOfTracks: $0.total_tracks,
+                                            artistName: $0.artists.first?.name ?? "-")
+        })))
+        
+        sections.append(.featuredPlaylists(viewModels: playlists.compactMap({
+            return FeaturedPlaylistCellViewModel(name: $0.name, artworkURL: URL(string: $0.images.first?.url ?? ""), creatorName: $0.owner.display_name)
+        })))
+        
+        sections.append(.recommendedTracks(viewModels: tracks.compactMap({
+            return RecommendedTrackCellViewModel(name: $0.name, aristName: $0.artists.first?.name ?? "-", artworkURL: URL(string: $0.album.images.first?.url ?? ""))
+        })))
+        
+        collectionView.reloadData()
+    }
+    
+    
+    private static func createSectionLayout(section: Int) -> NSCollectionLayoutSection {
+        
+        switch section {
+            
+        case 0:
+            let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
+            item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+            
+            let verticalGroup = NSCollectionLayoutGroup.vertical(
+                layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.9), heightDimension: .absolute(390)),
+                subitem: item,
+                count: 3)
+            
+            let horizontalGroup = NSCollectionLayoutGroup.horizontal(
+                layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.9), heightDimension: .absolute(390)),
+                subitem: verticalGroup,
+                count: 1)
+            
+            let section = NSCollectionLayoutSection(group: horizontalGroup)
+            section.orthogonalScrollingBehavior = .groupPaging
+            return section
+            
+            // ---------------------------------------------------------------
+            
+        case 1:
+            let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .absolute(200), heightDimension: .absolute(200)))
+            item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+            
+            let verticalGroup = NSCollectionLayoutGroup.vertical(
+                layoutSize: NSCollectionLayoutSize(widthDimension: .absolute(200), heightDimension: .absolute(400)),
+                subitem: item,
+                count: 2)
+            
+            let group = NSCollectionLayoutGroup.horizontal(
+                layoutSize: NSCollectionLayoutSize(widthDimension: .absolute(200), heightDimension: .absolute(400)),
+                subitem: verticalGroup,
+                count: 1)
+            
+            let section = NSCollectionLayoutSection(group: group)
+            section.orthogonalScrollingBehavior = .continuous
+            return section
+            
+            // ---------------------------------------------------------------
+            
+        case 2:
+            let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
+            item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+            
+            let group = NSCollectionLayoutGroup.vertical(
+                layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(80)),
+                subitem: item,
+                count: 1)
+            
+            let section = NSCollectionLayoutSection(group: group)
+            return section
+            
+            // ---------------------------------------------------------------
+            
+        default:
+            let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.9), heightDimension: .fractionalHeight(1.0)))
+            item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+            
+            let group = NSCollectionLayoutGroup.vertical(
+                layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.9), heightDimension: .absolute(390)),
+                subitem: item,
+                count: 1)
+            
+            let section = NSCollectionLayoutSection(group: group)
+            return section
+        }
     }
     
     @IBAction func profileTapped(_ sender: UIButton) {
         self.performSegue(withIdentifier: Constants.Segues.HOME_TO_SETTINGS, sender: self)
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return sections.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        let type = sections[section]
+        switch type {
+        case .newReleases(let viewModels):
+            return viewModels.count
+        case .featuredPlaylists(let viewModels):
+            return viewModels.count
+        case .recommendedTracks(let viewModels):
+            return viewModels.count
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
+        let type = sections[indexPath.section]
+        switch type {
+            
+        case .newReleases(let viewModels):
+            
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: NewReleaseCollectionViewCell.identifier, for: indexPath) as? NewReleaseCollectionViewCell else {
+                return UICollectionViewCell()
+            }
+            let viewModel = viewModels[indexPath.row]
+            cell.configure(with: viewModel)
+            return cell
+            
+        case .featuredPlaylists(let viewModels):
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FeaturedPlaylistCollectionViewCell.identifier, for: indexPath) as? FeaturedPlaylistCollectionViewCell else {
+                return UICollectionViewCell()
+            }
+            cell.configure(with: viewModels[indexPath.row])
+            return cell
+            
+        case .recommendedTracks(let viewModels):
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RecommendedTrackCollectionViewCell.identifier, for: indexPath) as? RecommendedTrackCollectionViewCell else {
+                return UICollectionViewCell()
+            }
+            cell.configure(with: viewModels[indexPath.row])
+            return cell
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        let section = sections[indexPath.section]
+        switch section {
+        case .featuredPlaylists:
+            self.clickedPlaylist = playlists[indexPath.row]
+            self.performSegue(withIdentifier: Constants.Segues.HOMEPAGE_TO_PLAYLIST, sender: self)
+        case .recommendedTracks:
+            break
+        case .newReleases:
+            self.clickedAlbum = newAlbums[indexPath.row]
+            self.performSegue(withIdentifier: Constants.Segues.HOMEPAGE_TO_ALBUM, sender: self)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case Constants.Segues.HOMEPAGE_TO_ALBUM:
+            let destinationVC = segue.destination as! AlbumViewController
+            destinationVC.album =  clickedAlbum
+        case Constants.Segues.HOMEPAGE_TO_PLAYLIST:
+            let destinationVC = segue.destination as! PlaylistViewController
+            destinationVC.playlist =  clickedPlaylist
+        default:
+            break
+        }
+        
     }
 }
