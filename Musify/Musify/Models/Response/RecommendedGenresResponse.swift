fdc359f3ab1cef21af323ceddec3dbe9e5769fcb
noamkuMoDev <noamk@moveo.co.il>
2022-07-03 14:05:26 +0300

    Re-organized files

---
+++ Musify/Musify/Models/Response/RecommendedGenresResponse.swift
@@ -0,0 +1,12 @@
+//
+//  RecommendedGenresResponse.swift
+//  Musify
+//
+//  Created by Noam Kurtzer on 30/06/2022.
+//
+
+import Foundation
+
+struct RecommendedGenresResponse: Codable {
+    let genres: [String]
+}
